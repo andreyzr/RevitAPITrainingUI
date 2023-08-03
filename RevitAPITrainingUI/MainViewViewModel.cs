25b147e8a12992782937d9b4af7a4e512733834e
andreyzr43 <andreyzr43@yandex.ru>
2023-08-03 17:57:04 +0300

    Добавьте файлы проекта.

---
+++ RevitAPITrainingUI/MainViewViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Autodesk.Revit.Creation;
+using Autodesk.Revit.UI;
+using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITrainingUI
+{
+    public class MainViewViewModel
+    {
+        private ExternalCommandData _commandData;
+
+        public DelegateCommand SelectCommand { get; private set; }
+
+        public MainViewViewModel(ExternalCommandData commandData)
+        {
+            _commandData = commandData;
+            SelectCommand = new DelegateCommand(OnSelectCommand);
+        }
+
+        public event EventHandler CloseRequest;
+        private void RaiseCloseRequest()
+        {
+            CloseRequest?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void OnSelectCommand()
+        {
+           RaiseCloseRequest();
+            
+
+            UIApplication uiapp = _commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            va
+        }
+    }
+}
