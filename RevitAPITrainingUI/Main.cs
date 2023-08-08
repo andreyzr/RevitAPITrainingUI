8114e94df5ac87145f59ab94a7c43b39c8f192dc
andreyzr43 <andreyzr43@yandex.ru>
2023-08-08 13:28:03 +0300

    1

--- RevitAPITrainingUI/Main.cs
+++ RevitAPITrainingUI/Main.cs
@@ -8,14 +8,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RevitAPITrainingUI
+namespace RevitAPITrainingId
 {
     [Transaction(TransactionMode.Manual)]
     public class Main : IExternalCommand
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            var window=new MainView(commandData);
+            var window=new MainView(commandData);//
+
             window.ShowDialog();
 
             return Result.Succeeded;
