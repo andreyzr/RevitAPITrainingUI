21a65821df6e5d306cae1a62af24b5494bad9b0c
andreyzr43 <andreyzr43@yandex.ru>
2023-08-09 07:55:47 +0300

    1

---
+++ HW5.1Info/Main.cs
@@ -0,0 +1,27 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW5._1Info
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ HW5.1Info/MainViewViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW5._1Info
+{
+    public class MainViewViewModel
+    {
+    }
+}
